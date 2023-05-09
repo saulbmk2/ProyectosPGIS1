2797a42061970a40189156dcaaddcb45324362a2
saulbmk2 <saul.marton.187@eaceuta.com>
2023-05-09 12:45:16 +0200

    Proyecto Calculadora
    Calculadora que previsualiza números hexadecimales a color.

---
+++ Calculadora/js/estilo.js
@@ -0,0 +1,23 @@
+const botones = document.querySelectorAll('.btn');
+const resultado = document.getElementById('resultado');
+const color = document.getElementById('color');
+
+botones.forEach(button => {
+    button.addEventListener('click', () => {
+
+        const value = button.value;
+            if (value === 'CE') {
+                resultado.value = '';
+                color.style.backgroundColor = '';
+            } 
+
+            else {
+                resultado.value += value;
+                if(resultado.value.length > 6){
+                    resultado.value = resultado.value.substring(0, 6);
+                }
+                color.style.backgroundColor = "#" + resultado.value;
+            }
+        
+        });
+});
